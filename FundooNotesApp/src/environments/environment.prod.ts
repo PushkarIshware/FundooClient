b78fc6571271f83c7d761b07c80aae00ed1a0eb9
Pushkar Ishware <40381903+PushkarIshware@users.noreply.github.com>
2019-04-12 14:52:58 +0530

    Update environment.prod.ts

--- FundooNotesApp/src/environments/environment.prod.ts
+++ FundooNotesApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://127.0.0.1:8000/api/'
-  // baseUrl: 'http://13.232.90.249/api/'
+  //baseUrl: 'http://127.0.0.1:8000/api/'
+  baseUrl: 'http://13.232.90.249/api/'
 };
